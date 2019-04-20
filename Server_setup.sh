6b2ef3ef5504b0ccc4b0fc0896e5cd1b826f87dd
Rihards Teteris <richitislv@gmail.com>
2019-04-20 19:30:21 +0300

    Move to new server

--- Server_setup.sh
+++ Server_setup.sh
@@ -5,14 +5,14 @@ terraform apply -auto-approve # To get output IP
 # cd ~/Code/CloudProject/cloud_project_ansible
 # check that variables are ok in Playbook
 
-ssh-keygen -f "/home/rihards/.ssh/known_hosts" -R "163.172.162.100"
+ssh-keygen -f "/home/rihards/.ssh/known_hosts" -R "95.216.206.226"
 cd ~/Code/CloudProject/cloud_project_ansible
 docker run -ti --rm -v ~/Code/CloudProject/cloud_project_ansible:/d -v ~/Code/CloudProject/Secrets/:/Secrets/ -v ~/.ssh/:/root/.ssh -w /d williamyeh/ansible:alpine3-onbuild sh -c 'apk add --no-cache openssh-client && \
 eval "$(ssh-agent -s)"; ssh-add /root/.ssh/scaleway && \
 ansible-playbook -i ./Inventory --limit production --vault-password-file ../Secrets/ansible_vault_pass -u root --diff Playbook.yml -e ansible_port=22'
 
 cd ~/Code/work_in_progress/Kubernetes
-SERVER_IP="163.172.162.100"; ssh ${SERVER_IP} -p 3013
+SERVER_IP="95.216.206.226"; ssh ${SERVER_IP} -p 3013
 mkdir .kube
 sudo cp /etc/kubernetes/admin.conf ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
@@ -21,6 +21,7 @@ sudo mkdir /acme
 exit
 
 SERVER_IP="163.172.162.100"
+SERVER_IP="95.216.206.226"
 scp -P 3013 ${SERVER_IP}:/home/rihards/.kube/config ~/.kube/config
 
 ### Helm
@@ -30,7 +31,7 @@ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admi
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
 cd ~/Code/CloudProject/cloud_project_kubernetes/Traefik
-kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/containous/traefik/v1.7/examples/k8s/traefik-rbac.yaml
 kubectl apply -f traefik-configmap.yml
 kubectl apply -f traefik-deployment.yml
 kubectl apply -f traefik-ui.yml
@@ -38,8 +39,8 @@ kubectl apply -f cheese.yml
 
 # Netdata
 cd ~/Code/CloudProject/cloud_project_kubernetes/Netdata
-kubectl apply -f netdata-back.yml
 kubectl apply -f netdata-pub.yml
+kubectl apply -f netdata-back.yml
 
 ### Nextcloud
 cd ~/Code/Kubes/nextcloud
@@ -48,7 +49,7 @@ cd ~/Code/Kubes/nextcloud
 
 
 ### Add nodes
-SERVER_IP="163.172.162.100"; ssh ${SERVER_IP} -p 3013
+SERVER_IP="95.216.206.226"; ssh ${SERVER_IP} -p 3013
 kubeadm token create --print-join-command
 exit
 
