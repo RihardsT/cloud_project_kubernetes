c85f1376339f7eb0a739ebc21427176363ac3e99
Rihards <richitislv@gmail.com>
2020-02-17 10:38:23 +0200

    WIP logstash stuff

--- Elastic/commands_logstash.sh
+++ Elastic/commands_logstash.sh
@@ -8,55 +8,10 @@ filter {
 
 
 
-- pipeline.id: upstream
-  config.string: |
-    input {
-      http {
-        codec => line
-        host => "0.0.0.0"
-        port => "8080"
-      }
-    }
-    output {
-      pipeline {
-        if [@metadata][beat] in [pipeline.id] {
-          send_to => [@metadata][beat]
-        }
-        else {
-          send_to => [myVirtualAddress]
-        }
-      }
-    }
-- pipeline.id: downstream
-  config.string: |
-    input {
-      pipeline {
-        address => myVirtualAddress
-      }
-    }
-    filter {
-      json {
-        skip_on_invalid_json => true
-        source => "message"
-        target => "parsedJson"
-        id => "%{[parsedJson][index]}"
-        add_field => { "index" => "%{[parsedJson][index]}" }
-        remove_field => [ "parsedJson" ]
-      }
-      if [index] == '%{[parsedJson][index]}' {
-        mutate {
-          update => { "index" => "http-%{+YYYY.MM}" }
-        }
-      }
-    }
-    output {
-      elasticsearch {
-        hosts => "elasticsearch:9200"
-        index => "%{index}"
-      }
-      stdout {
-        codec  => rubydebug {
-          metadata => true
-        }
-      }
-    }
+curl -X POST http://logstash:8080/ \
+  -H 'content-type: application/json' \
+  -d '{"message": "testindex", "index": "testindex"}'
+
+
+
+kubectl -n elastic annotate --overwrite pods logstash-65c9fbd69f-h28vf bump=2
