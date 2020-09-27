5639608c464254af43008148d63ca047bed7c64c
Rihards Teteris <richitislv@gmail.com>
2020-09-27 22:40:52 +0300

    Filebeat dissect test

--- Server_setup.sh
+++ Server_setup.sh
@@ -89,7 +89,7 @@ sshuttle -e 'ssh -i ~/.ssh/hetzner' --python '/usr/bin/python3' -r rihards@95.21
 
 ####### Kubernetes upgrade
 sudo kubeadm upgrade plan
-sudo kubeadm upgrade apply v1.18.2
+sudo kubeadm upgrade apply v1.19.2
 
 # For upgrade to v1.17.0 had to use --ignore-preflight-errors=all
 # Likely because I only have single master node
