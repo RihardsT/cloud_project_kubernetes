b507f13a7fdebf9356a8cac53bad705c33a1663e
Rihards Teteris <richitislv@gmail.com>
2021-07-28 00:31:10 +0300

    Notes on Gitlab - restart Gitlab to get it working
    Backup Gitlab
    Remove gitlab.rb, which is already moved to configmap

--- Gitlab/commands.sh
+++ Gitlab/commands.sh
@@ -1,7 +1,18 @@
-kubectl exec -it $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep gitlab) -- bash
-gitlab-backup create
+### Backup
+kubectl exec -it $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep gitlab) -- bash -c 'gitlab-backup create'
 
-ssh -o "StrictHostKeyChecking=no" $(terraform output -state=/home/rihards/Code/cloud_project/cloud_project_terraform_gcp/terraform.tfstate | grep -oP '"\K[^"]+')
-sudo s3cmd put /data/gitlab/data/backups/$(ls /data/gitlab/data/backups/) s3://rudenspavasaris
+ssh -o "StrictHostKeyChecking=no" $(terraform output -state=/home/rihards/Code/cloud_project/cloud_project_terraform_gcp/terraform.tfstate | grep -oP '"\K[^"]+') 'sudo s3cmd put /data/gitlab/data/backups/$(sudo ls /data/gitlab/data/backups/) /data/gitlab/config/gitlab-secrets.json s3://rudenspavasaris'
 
-Copy the gitlab-secrets.json too
+###
+# For some reason, Gitlab is not accessible from the beggining, when the VM is created.
+# Container restart fixes that.
+kubectl scale deployment gitlab --replicas=0
+kubectl scale deployment gitlab --replicas=1
+
+# Check if you can access it from another container
+kubectl exec -it $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep nextcloud) -- bash -c 'curl -k https://gitlab'
+
+# To trigger certificate generation, remove the previously generated certificates,
+# to re-trigger Letsencrypt
+kubectl exec -it $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep gitlab) -- bash -c 'rm /etc/gitlab/ssl/gitlab* && gitlab-ctl reconfigure'
+kubectl logs --follow $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep gitlab)
--- Gitlab/gitlab.rb
+++
@@ -1,6 +0,0 @@
-##! Check out the latest version of this file to know about the different
-##! settings that can be configured, when they were introduced and why:
-##! https://gitlab.com/gitlab-org/omnibus-gitlab/blame/master/files/gitlab-config-template/gitlab.rb.template
-
-external_url "https://gitlab.rudenspavasaris.id.lv"
-gitlab_rails['gitlab_shell_ssh_port'] = 4422
