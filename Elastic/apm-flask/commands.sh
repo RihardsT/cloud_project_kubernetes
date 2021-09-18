46f2d23a056b8f041d64e180f3bbe50ce97f57a5
Rihards Teteris <richitislv@gmail.com>
2021-09-18 14:16:43 +0300

    Elastic APM RUM, and trying PersistentVolume
    Persistent volumes didn't give the benefit I wanted.
    Might be the local type is just not what I need.
    Still have to make sure that permissions are set for user id 1000,
    to have elasticsearch working properly, even with PV.
    But meh, that's not something that I need urgently for now.

--- Elastic/apm-flask/commands.sh
+++ Elastic/apm-flask/commands.sh
@@ -6,3 +6,10 @@ while true; do
   curl localhost:5000 &>/dev/null
   sleep 2
 done
+
+# For dirty testing to force a "different client":
+docker run --rm -ti --name apm_test ubuntu bash
+apt update && apt install -y python3 python3-pip curl vim
+pip3 install flask elastic-apm[flask]
+# Then just run it as usual
+docker exec -ti apm_test bash
---
+++ Elastic/apm-rum/commands.sh
@@ -0,0 +1,5 @@
+https://github.com/elastic/apm-agent-rum-js/releases
+
+How to run this?
+Set up your Elastic stack as usual and then just open this file in browser.
+It's fine. Plain html works right away, no need to set up webserver for this.
