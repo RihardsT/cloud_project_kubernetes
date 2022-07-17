4fde84a0e89093fe791e547c1d2cc562231121d5
Rihards Teteris <richitislv@gmail.com>
2022-07-17 19:45:17 +0300

    Kind Nginx Ingress

--- Kind/commands.sh
+++ Kind/commands.sh
@@ -22,3 +22,17 @@ iptable_nat
 
 cd ~/Code/cloud_project_kubernetes/Kind
 kind create cluster --config=${PWD}/config.yml
+##### Ingress Nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/usage.yaml # Example
+curl localhost:8080/foo
+
+
+### Clean up
+kind delete cluster # cluster is the default name
+
+### Useful commands
+kubectl config get-contexts
+kubectl config use-context CONTEXT # to switch to real K8s cluster
+
+echo "127.0.0.1 kind.local" | sudo tee -a /etc/hosts
