51914cdaa9be5f529faff9994f3aedbb22ac6eb6
Rihards <rudenspavasaris@proton.me>
2025-01-06 00:15:46 +0200

    WIP Radicale - set up auth, storage to do

--- Other_WIP/Radicale/commands.sh
+++ Other_WIP/Radicale/commands.sh
@@ -1,3 +1,7 @@
+### Docs:
+https://radicale.org/v3.html
+
+### Build image
 git clone https://github.com/Kozea/Radicale.git
 cd Radicale
 sudo nerdctl run -d --name buildkitd --privileged -v ${PWD}:/d -w /d moby/buildkit:latest
@@ -6,3 +10,13 @@ sudo nerdctl load -i radicale.tar
 sudo nerdctl rm -f buildkitd
 
 sudo nerdctl run --rm -ti -p 5232:5232 --name radicale radicale
+
+### Apply in kubernetes
+sudo nerdctl -n k8s.io load -i radicale.tar
+kubectl create secret generic radicale-users --from-literal=users=$(cat secrets_users)
+kubectl apply -f radicale.yaml
+
+
+### Delete
+k delete secret radicale-users
+k delete -f radicale.yaml
