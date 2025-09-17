4f73537246dbe2ba5545b308a9f75d06be58d20b
Rihards <rudenspavasaris@proton.me>
2025-09-17 22:20:28 +0300

    Traefik 3 with helm

--- Base/Traefik/commands.sh
+++ Base/Traefik/commands.sh
@@ -33,3 +33,20 @@ https://observatory.mozilla.org/analyze/vieta.rudenspavasaris.id.lv
 
 # For HTTP3? https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size
 sysctl -w net.core.rmem_max=2500000
+
+
+
+### Install Traefik 3 with Helm
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+helm install traefik traefik/traefik -f helm-values.yaml --wait
+
+# Upgrade
+# https://github.com/traefik/traefik-helm-chart/tree/master?tab=readme-ov-file#upgrading
+helm upgrade traefik traefik/traefik -f helm-values.yaml
+# To make sure new config is loaded, delete the old pod
+kubectl delete pod $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep traefik)
+
+# TODO
+Get Traefik working with port 80 and 443
