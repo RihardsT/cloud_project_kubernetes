6fa87bcf68bfaa67d7eaacf8f30ab7509f9f2eed
Rihards <rudenspavasaris@proton.me>
2025-09-24 22:02:15 +0300

    Traefik 3, got it working with TLS and on 80 and 443 with iptables

--- Base/Traefik/commands.sh
+++ Base/Traefik/commands.sh
@@ -1,39 +1,9 @@
-https://github.com/traefik/traefik/tree/master/docs/content/reference/dynamic-configuration
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3.4/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
-
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
-kubectl apply -f ~/Code/cloud_project/cloud_project_kubernetes/Base/Traefik_v2/
-
-# Dashboard
-https://traefik.rudenspavasaris.id.lv/dashboard/
-
-# Further use IngressRoute like so:
----
-apiVersion: traefik.containo.us/v1alpha1
-kind: IngressRoute
-metadata:
-  name: ingressroutetls
-spec:
-  entryPoints:
-    - https
-  routes:
-  - match: Host(`rudenspavasaris.id.lv`)
-    kind: Rule
-    services:
-    - name: stilton
-      port: 80
-  tls:
-    certResolver: default
-
-
-
-https://observatory.mozilla.org/analyze/vieta.rudenspavasaris.id.lv
-
-# For HTTP3? https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size
-sysctl -w net.core.rmem_max=2500000
-
+# TODO
+- [x] Get Traefik working with port 80 and 443
+  Workaround with iptables
+  sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8000
+  sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 6443
+- [ ] Get HTTPS working
 
 
 ### Install Traefik 3 with Helm
@@ -41,12 +11,16 @@ helm repo add traefik https://traefik.github.io/charts
 helm repo update
 
 helm install traefik traefik/traefik -f helm-values.yaml --wait
-
 # Upgrade
 # https://github.com/traefik/traefik-helm-chart/tree/master?tab=readme-ov-file#upgrading
-helm upgrade traefik traefik/traefik -f helm-values.yaml
-# To make sure new config is loaded, delete the old pod
-kubectl delete pod $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep traefik)
+# To make sure new config is loaded, delete the old pod also
+helm upgrade --install traefik traefik/traefik -f helm-values.yaml && \
+kubectl delete pod $(kubectl get pods --no-headers -o custom-columns=":metadata.name" --sort-by=.metadata.creationTimestamp | grep traefik | head -n 1)
+# kubectl delete pod $(kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep traefik)
 
-# TODO
-Get Traefik working with port 80 and 443
+###
+helm uninstall traefik
+
+
+# Check what helm would apply
+helm template traefik traefik/traefik -f helm-values.yaml
---
+++ Base/Traefik/v2/commands.sh
@@ -0,0 +1,32 @@
+https://github.com/traefik/traefik/tree/master/docs/content/reference/dynamic-configuration
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+kubectl apply -f ~/Code/cloud_project/cloud_project_kubernetes/Base/Traefik/v2/
+
+# Dashboard
+https://traefik.rudenspavasaris.id.lv/dashboard/
+
+# Further use IngressRoute like so:
+---
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
+  name: ingressroutetls
+spec:
+  entryPoints:
+    - https
+  routes:
+  - match: Host(`rudenspavasaris.id.lv`)
+    kind: Rule
+    services:
+    - name: stilton
+      port: 80
+  tls:
+    certResolver: default
+
+
+
+https://observatory.mozilla.org/analyze/vieta.rudenspavasaris.id.lv
+
+# For HTTP3? https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size
+sysctl -w net.core.rmem_max=2500000
